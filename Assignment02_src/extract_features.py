80fcbcb4d579f79049f6878b3dd6980b3c27832b
CSEKU160212 <lotif1612@cseku.ac.bd>
2019-07-20 16:40:32 +0600

    Assignment02

--- Assignment02_src/extract_features.py
+++ Assignment02_src/extract_features.py
@@ -9,17 +9,18 @@ import xlsxwriter
 os.chdir("/home/cseku160212/PycharmProjects/DataMining/Train and test ETH 80 dataset/TrainETH80data2952")
 allImages = sorted(os.listdir("."))
 
-outputDir = r"/home/cseku160212/PycharmProjects/DataMining/Assignment01Output"
+outputDir = r"/home/cseku160212/PycharmProjects/DataMining/Assignment02_Output"
 os.chdir(outputDir)
 
-workbook = xlsxwriter.Workbook("Assignment01_Mean_SD_OneByOneInsertion.xlsx")
+workbook = xlsxwriter.Workbook("Assignment02_output.xlsx")
 worksheet = workbook.add_worksheet()
 
 bold = workbook.add_format({'bold': True})
 
 worksheet.write('A1', 'Label', bold)
 worksheet.write('B1', 'Mean', bold)
-worksheet.write('C1', 'Standard Deviation', bold)
+worksheet.write('C1', 'Median', bold)
+worksheet.write('D1', 'Midrange', bold)
 
 row = 1
 column = 0
@@ -31,18 +32,18 @@ for eachImage in allImages:
 
     label = re.split('1|2|3|4|5|6|7|8|9|-', eachImage)
     mean = np.mean(imageIntensityArray)
-    sd = np.std(imageIntensityArray)
+    median = np.median(imageIntensityArray)
+    min = np.min(imageIntensityArray)
+    max = np.max(imageIntensityArray)
+    midrange = (min+max) / 2
 
     os.chdir(outputDir)
     worksheet.write(row, column, label[0])
     worksheet.write(row, column + 1, mean)
-    worksheet.write(row, column + 2, sd)
+    worksheet.write(row, column + 2, median)
+    worksheet.write(row, column + 3, midrange)
 
-    print("For Image", eachImage, "Mean = ", mean, "\tStandard Deviation: ", sd)
+    print("For Image", eachImage, "Mean = ", mean, "\tmedian: ", median, "\tmidrange:", midrange)
     row += 1
 
 workbook.close()
-
-
-
-
